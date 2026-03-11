8bbedf2b0a36b56a0b64474ae349c1c8c15702ac
SanoMush <mustofahusni27@gmail.com>
2026-03-11 18:56:08 +0700

    first commit

---
+++ app/src/main/java/com/sanomush/tari/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.sanomush.tari
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Handle padding untuk EdgeToEdge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Langsung arahkan ke ChatFragment (F-01: Zero-login screen)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                // Nanti kita buat file fragment_chat.xml dan class ChatFragment
+                .replace(R.id.main, ChatFragment())
+                .commit()
+        }
+    }
+}
