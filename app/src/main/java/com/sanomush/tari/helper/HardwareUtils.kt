529b94f5c2384f9bb1dc3e4eaf391449a7b5b5fb
SanoMush <mustofahusni27@gmail.com>
2026-03-13 21:26:15 +0700

    Add Hardware Utils

--- app/src/main/java/com/sanomush/tari/helper/HardwareUtils.kt
+++ app/src/main/java/com/sanomush/tari/helper/HardwareUtils.kt
@@ -1,4 +1,25 @@
 package com.sanomush.tari.helper
 
-class HardwareUtils {
+import android.content.Context
+import android.hardware.camera2.CameraManager
+
+object HardwareUtils {
+    private var isFlashlightOn = false
+
+    fun toggleFlashlight(context: Context): Boolean {
+        return try {
+            val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            // Kamera belakang utama biasanya ada di index 0
+            val cameraId = cameraManager.cameraIdList[0]
+
+            isFlashlightOn = !isFlashlightOn
+            cameraManager.setTorchMode(cameraId, isFlashlightOn)
+
+            // Mengembalikan status senter saat ini (nyala/mati)
+            isFlashlightOn
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
 }
